6c6aab8562450fe889d972957e2980a767b6b53e
Peter Kurpiewski <pkurpiewski@gmail.com>
2015-04-16 10:28:52 -0400

    another exercise from chapter 2

---
+++ chapter 2/section 2/deep-reverse.scm
@@ -0,0 +1,16 @@
+(define testlist (list (list 1 2) (list 3 4)))
+(define z (list (list 1 2) (list 3 (list 5 8))))
+
+(define (reverse items)
+  (if (null? items)
+      '()
+      (append (reverse (cdr items))
+	      (list (car items)))))
+
+(define (deep-reverse items)
+  (cond ((null? items) '())
+	((pair? (car items)) (append (deep-reverse (cdr items))
+				     (list (deep-reverse (car items)))))
+	(else (append (deep-reverse (cdr items))
+		      (list (car items))))))
+
